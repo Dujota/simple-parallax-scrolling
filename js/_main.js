0668f60076987024e423da7cf07176644498d18b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-07 11:00:26 -0500

    finished simple scrolling effect

--- js/_main.js
+++ js/_main.js
@@ -1,5 +1,7 @@
 // Init Skrollr
-var s = skrollr.init();
+var s = skrollr.init({
+  forceHeight: false
+});
 
 // Refresh Skrollr after resizing our sections
 s.refresh($('.homeSlide'));
@@ -29,7 +31,9 @@ s.refresh($('.homeSlide'));
 
 		// Init Skrollr
 
-    // var skrollr = skrollr.init();
+    // var skrollr = skrollr.init({
+    //   forceHeight = false
+    // });
 
 		// Get window size
 	    winH = $window.height();
